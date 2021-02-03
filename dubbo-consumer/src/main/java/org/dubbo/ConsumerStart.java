298165b55ea6b159d28e28d91c09134b6d5ee2c0
t-yuanyu.wang <yuanyu.wang@egreatwall.com>
2021-02-03 16:26:38 +0800

    feat：添加nacos ,修改springboot版本 2.3.x;

--- dubbo-consumer/src/main/java/org/dubbo/ConsumerStart.java
+++ dubbo-consumer/src/main/java/org/dubbo/ConsumerStart.java
@@ -3,6 +3,9 @@ package org.dubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+
+@NacosPropertySource(dataId = "dubbo.application.yml",groupId = "DEFAULT_GROUP" , autoRefreshed = true)
 @SpringBootApplication
 public class ConsumerStart {
 
