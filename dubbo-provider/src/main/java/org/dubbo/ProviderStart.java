59b9bdaa42c2ef460825986ba5a745152b61ea79
t-yuanyu.wang <yuanyu.wang@egreatwall.com>
2021-02-03 16:33:08 +0800

    feat:provider 添加nacos

--- dubbo-provider/src/main/java/org/dubbo/ProviderStart.java
+++ dubbo-provider/src/main/java/org/dubbo/ProviderStart.java
@@ -3,7 +3,11 @@ package org.dubbo;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+
 @EnableDubbo
+@NacosPropertySource(dataId = "dubbo.application.yml",groupId = "DEFAULT_GROUP" , autoRefreshed = true)
 @SpringBootApplication
 public class ProviderStart {
 
